55711767603eab23cd5321259ab9cd9acb3361d6
anas-raza~ <anusraza268@gmail.com>
2024-05-26 11:46:17 -0700

    modified

--- app.js
+++ app.js
@@ -226,8 +226,65 @@ const products = [
 // console.log(products);
 
 const div=document.querySelector('div')
-for (let i = 0; i < products.length; i++) {
-    console.log(products[i]);
-    div.innerHTML +=`<h1>$produts{i}</h1>`
+// for (let i = 0; i < products.length; i++) {
+//     console.log(products[i]);
+//     div.innerHTML +=`<h1>name:${products[i].name}</h1>
+//  <h5>price:${products[i].price}</h5>
+//     <h5>category:${products[i].category}</h5>
+//     <h5>brand:${products[i].brand}</h5>
     
+//     `
+    
+// }
+// const Electronics=products.filter(item=>{
+//     return item.category ==='Electronics' && item.price > 100
+// }).filter((item)=>{
+// return item.price <200
+
+// })
+
+//  console.log(Electronics);
+// const Electronics =products.filter(item=>{
+//     console.log(item.category);
+// })
+
+
+// products.map((item)=>{
+// // console.log(item.category);
+// div.innerHTML +=`<h1>name:${item[i].name}</h1>
+//  <h5>price:${item[i].price}</h5>
+//    <h5>category:${item[i].category}</h5>
+//   <h5>brand:${item[i].brand}</h5>`
+// })
+
+const filteredItem=(btn)=>{
+    div.innerHTML=``
+console.log(btn.innerHTML);
+const filteredItem=products.filter(item=>item .category === btn.
+    innerHTML).map(item =>{
+
+    div.innerHTML +=
+    `
+    <h1>name:${item.name}</h1>
+  <h5>price:${item.price}</h5>
+   <h5>category:${item.category}</h5>
+  <h5>brand:${item.brand}</h5>`
+})
+
+console.log(filteredItem);
 }
+
+const seeall=(btn)=>{
+    
+// const category= btn.innerHTML
+    console.log(btn.innerHTML)
+    const seeall =products.filter(item=>item.category === btn.innerHTML).map(item =>{
+            div.innerHTML +=`<h1>name:${item.name}</h1>
+//  <h5>price:${item.price}</h5>
+//    <h5>category:${item.category}</h5>
+//   <h5>brand:${item.brand}</h5>`
+        })
+        console.log(seeall);
+    }
+        
+        
